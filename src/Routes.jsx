0afa9bce16997148835c4393e2e771feeff84cd0
adarsha777 <chaulagainaadarsha7@gmail.com>
2024-07-29 20:27:52 +0545

    new way to implement homepage

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Homepage";
+import Home from "./pages/Homepage/Homepage";
 
 export const MainRoute = createBrowserRouter([
   {
