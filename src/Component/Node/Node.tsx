e0be02995117656450798840053859c33cef2411
adarsha777 <chaulagainaadarsha7@gmail.com>
2024-07-31 12:51:34 +0545

    Added node

--- src/Component/Node/Node.tsx
+++ src/Component/Node/Node.tsx
@@ -1,46 +1,46 @@
-// import React from "react";
-// import "./Node.css";
+import React from "react";
+import "./Node.css";
 
-// export interface NodeProps {
-//   row: number;
-//   col: number;
-//   isFinish: boolean;
-//   isStart: boolean;
-//   isWall: boolean;
-//   onMouseUp: any;
-//   onMouseDown: any;
-//   onMouseEnter: any;
-//   distance?: any;
-//   isVisited?: boolean;
-// }
+export interface NodeProps {
+  row: number;
+  col: number;
+  isFinish: boolean;
+  isStart: boolean;
+  isWall: boolean;
+  onMouseUp: any;
+  onMouseDown: any;
+  onMouseEnter: any;
+  distance?: any;
+  isVisited?: boolean;
+}
 
-// const Node = ({
-//   row,
-//   col,
-//   isFinish,
-//   isWall,
-//   isStart,
-//   onMouseUp,
-//   onMouseEnter,
-//   onMouseDown,
-// }: NodeProps) => {
-//   const extraClassName = isFinish
-//     ? "node-finish"
-//     : isStart
-//     ? "node-start"
-//     : isWall
-//     ? "node-wall"
-//     : "";
-//   return (
-//     <div
-//       id={`node-${row}-${col}`}
-//       className={`node ${extraClassName}`}
-//       onMouseDown={() => onMouseDown(row, col)}
-//       onMouseEnter={()=> onMouseEnter(row, col)}
-//       onMouseUp={() => onMouseUp(row, col)}
+const Node = ({
+  row,
+  col,
+  isFinish,
+  isWall,
+  isStart,
+  onMouseUp,
+  onMouseEnter,
+  onMouseDown,
+}: NodeProps) => {
+  const extraClassName = isFinish
+    ? "node-finish"
+    : isStart
+    ? "node-start"
+    : isWall
+    ? "node-wall"
+    : "";
+  return (
+    <div
+      id={`node-${row}-${col}`}
+      className={`node ${extraClassName}`}
+      onMouseDown={() => onMouseDown(row, col)}
+      onMouseEnter={()=> onMouseEnter(row, col)}
+      onMouseUp={() => onMouseUp(row, col)}
       
-//     ></div>
-//   );
-// };
+    ></div>
+  );
+};
 
-// export default Node;
+export default Node;
